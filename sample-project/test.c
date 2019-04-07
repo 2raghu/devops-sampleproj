5ca1634815fa057863f6681ae11e23afd80663f8
Raghuram Chary J <raghujindia@gmail.com>
2019-04-07 09:03:58 -0400

    Added size of struct and .gitignore to ignore *.o files

--- sample-project/test.c
+++ sample-project/test.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+struct q
+{
+	int a;
+	char b;
+	char c;
+};
+
 int main(void)
 {
+	struct q p;
 	printf("Hello  world !!!\n");
+	printf("size of struct q %ld !!!\n",sizeof(p));
+
 	return 0;
 }
